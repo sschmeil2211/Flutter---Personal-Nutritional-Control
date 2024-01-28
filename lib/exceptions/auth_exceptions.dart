c8229619ab2badde6bc77284f21bda55bd972ac9
sschmeil22 <sebaschmeil123@gmail.com>
2024-01-27 23:02:26 -0300

    recovery password function

--- lib/exceptions/auth_exceptions.dart
+++ lib/exceptions/auth_exceptions.dart
@@ -17,8 +17,18 @@ class AuthExceptions{
         return const AuthExceptions('This user has been disabled. Please contact support for help.');
       case 'user-not-found':
         return const AuthExceptions('There is no user corresponding to the email provided.');
+      case 'missing-android-pkg-name':
+        return const AuthExceptions('An Android package name must be provided if the Android app is required to be installed.');
       case 'wrong-password':
-        return const AuthExceptions('he password is invalid for the given email, or the account corresponding to the email does not have a password set.');
+        return const AuthExceptions('He password is invalid for the given email, or the account corresponding to the email does not have a password set.');
+      case 'missing-continue-uri':
+        return const AuthExceptions('A continue URL must be provided in the request.');
+      case 'missing-ios-bundle-id':
+        return const AuthExceptions('An iOS Bundle ID must be provided if an App Store ID is provided.');
+      case 'invalid-continue-uri':
+        return const AuthExceptions('The continue URL provided in the request is invalid.');
+      case 'unauthorized-continue-uri':
+        return const AuthExceptions('The domain of the continue URL is not whitelisted. Whitelist the domain in the Firebase console.'); 
       default:
         return const AuthExceptions();
     }
---
+++ lib/screens/recovery_password_screen.dart
@@ -0,0 +1,82 @@
+// ignore_for_file: curly_braces_in_flow_control_structures
+
+import 'package:flutter/material.dart';
+import 'package:personal_nutrition_control/providers/providers.dart';
+import 'package:personal_nutrition_control/utils/utils.dart';
+
+import 'package:personal_nutrition_control/widgets/widgets.dart';
+import 'package:provider/provider.dart';
+
+class RecoveryPasswordScreen extends StatelessWidget {
+  const RecoveryPasswordScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: const Icon(Icons.arrow_back_ios_new_rounded),
+        ),
+      ),
+      body: SafeArea(
+        child: RecoveryPassword(),
+      ),
+    );
+  }
+}
+
+class RecoveryPassword extends StatefulWidget {
+  const RecoveryPassword({super.key});
+
+  @override
+  State<RecoveryPassword> createState() => _RecoveryPasswordState();
+}
+
+class _RecoveryPasswordState extends State<RecoveryPassword> {
+
+  TextEditingController email = TextEditingController();
+  bool loading = false;
+
+  Future<void> onPressed(UserProvider userProvider) async {
+    if(email.text.isEmpty) return;
+    setState(() => loading = true);
+    String? message = await userProvider.recoveryPassword(email.text);
+    if(!context.mounted) return;
+    if(message == null)
+      Navigator.pop(context);
+    else{
+      setState(() => loading = false);
+      ScaffoldMessenger.of(context).showSnackBar(customSnackBar(message, Colors.deepOrange));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    UserProvider userProvider = Provider.of(context, listen: false);
+
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Enter your Email and we will send you a password reset link',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 16),
+          ),
+          InputField(
+            prefixIcon: Icons.email,
+            labelText: 'Email',
+            textEditingController: email
+          ),
+          ButtonIndicator(
+            isLoading: loading,
+            onPressed: () async => await onPressed(userProvider),
+            label: 'Send email'
+          )
+        ],
+      ),
+    );
+  }
+}
